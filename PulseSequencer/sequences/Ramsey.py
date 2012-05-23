94fb5f59370d601e955e895800363a77904d8c0d
Colm Ryan <cryan@bbn.com>
2012-05-22 20:47:34 -0400

    Add sequences package and sequences as functions.
    Turn the sequences folder into a package and automatically pull all the sequence functions into the namespace.
    --CAR

--- PulseSequencer/sequences/Ramsey.py
+++ PulseSequencer/sequences/Ramsey.py
@@ -1,5 +1,8 @@
-# Simple Ramsey experiment X90-delay-U90
+'''
+Simple Ramsey experiment X90-delay-U90
+'''
 
+#Imports necessary for compiling sequences
 from Channels import load_channel_info
 from PulseSequencer import compile_sequences, logical2hardware
 from PulseSequencePlotter import plot_pulse_seqs
@@ -7,37 +10,38 @@ from TekPattern import write_Tek_file
 
 import numpy as np
 
-#Channel boiler-plate
 
-#Load the channel information
-channelObjs, channelDicts = load_channel_info('ChannelParams.json') 
+def Ramsey(targetQubit = 'q1', fileName='Ramsey', pulseSpacings=np.linspace(0,10e-6,100), plotSeqs=True, readoutPulseLength=6e-6):
 
-q1 = channelObjs['q1']
-q2 = channelObjs['q2']
-digitizerTrig = channelObjs['digitizerTrig']
-measChannel = channelObjs['measChannel']
-
-channelDicts['AWGList'] = ['TekAWG1']
-
-
-#Define a typical sequence: say Pi Ramsey
-readoutBlock = digitizerTrig.gatePulse(100e-9)+measChannel.gatePulse(2e-6)
-def single_ramsey_sequence(pulseSpacing):
-    tmpSeq = [q1.X90(), q1.QId(pulseSpacing)+q2.X180(), q1.X90(), readoutBlock]
-    tmpSeq[1].alignment = 'centre'
-    return tmpSeq
-
-pulseSeqs = [single_ramsey_sequence(pulseSpacing) for pulseSpacing in np.linspace(1e-6,20e-6,100)]
-
-LLs, WFLibrary = compile_sequences(pulseSeqs)  
+    #Channel boiler-plate
+    #Load the channel information
+    channelObjs, channelDicts = load_channel_info('ChannelParams.json') 
     
-AWGWFs = logical2hardware(LLs, WFLibrary, channelDicts)
-
-
-print('Writing Tek File...')
-write_Tek_file(AWGWFs['TekAWG1'], 'silly.awg', 'silly')
-print('Done writing Tek File.')
-
-plot_pulse_seqs(AWGWFs)
-
+    targetQ = channelObjs[targetQubit]
+    digitizerTrig = channelObjs['digitizerTrig']
+    measChannel = channelObjs['measChannel']
+    
+    channelDicts['AWGList'] = ['TekAWG1']
+    
+    #Define a typical sequence: say Pi Ramsey
+    readoutBlock = digitizerTrig.gatePulse(100e-9)+measChannel.gatePulse(readoutPulseLength)
+    def single_ramsey_sequence(pulseSpacing):
+        tmpSeq = [targetQ.X90(), targetQ.QId(pulseSpacing), targetQ.X90(), readoutBlock]
+        tmpSeq[1].alignment = 'centre'
+        return tmpSeq
+    
+    #Create the list of pulse sequences
+    pulseSeqs = [single_ramsey_sequence(pulseSpacing) for pulseSpacing in pulseSpacings]
+    
+    #Complile to link lists
+    LLs, WFLibrary = compile_sequences(pulseSeqs)  
+        
+    AWGWFs = logical2hardware(LLs, WFLibrary, channelDicts)
+    
+    print('Writing Tek File...')
+    write_Tek_file(AWGWFs['TekAWG1'], 'silly.awg', 'silly')
+    print('Done writing Tek File.')
+    
+    if plotSeqs:
+        plot_pulse_seqs(AWGWFs)
 
