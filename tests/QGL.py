bcd43672cc69f1a6650a5ae9d728da6b3e1c4878
Colm Ryan <cryan@bbn.com>
2012-12-13 23:23:46 -0500

    Clean up relative imports
    If we're willing to run the unittests as as module (python -m PyQLab.test.QGL) then this works better.

--- tests/QGL.py
+++ tests/QGL.py
@@ -1,6 +1,8 @@
 import unittest
 import numpy as np
 
+from ..QGL import *
+
 
 class SingleQubit(unittest.TestCase):
 
@@ -34,20 +36,6 @@ class MultiQubit(unittest.TestCase):
 
 if __name__ == "__main__":
     
-    if  __package__ is None:
-        import sys, os
-        # The following assumes the script is in the top level of the package
-        # directory.  We use dirname() to help get the parent directory to add to
-        # sys.path, so that we can import the current package.  This is necessary 
-        # since when invoked directly, the 'current' package is not automatically
-        # imported.
-        parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        sys.path.insert(0, parent_dir)
-        import PyQLab
-        __package__ = "PyQLab"
-        del sys, os
-    from .QGL import *
-
     unittest.main()
 #    singleTest = unittest.TestSuite()
 #    singleTest.addTest(SingleQubit("Ramsey"))
