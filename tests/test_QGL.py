c3e7f1ee218d2d748727059ec02ce0d6611e412e
Blake R. Johnson <bjohnson@bbn.com>
2012-12-14 12:02:36 -0500

    Passing more tests.
    Now have a multi-sequence compiler. Need to finish mapping to hardware.
    --BRJ and CAR

--- tests/test_QGL.py
+++ tests/test_QGL.py
@@ -6,15 +6,26 @@ from QGL import *
 
 
 class SingleQubit(unittest.TestCase):
+    def setUp(self):
+        self.q1 = Qubit('q1', piAmp=1.0, pi2Amp=0.5, pulseLength=30e-9)
 
     def test_Ramsey(self):
         '''
         Test simple Ramsey sequence
         '''
-        q1 = Qubit('q1', piAmp=1.0, pi2Amp=0.5, pulseLength=30e-9)
+        q1 = self.q1
         ramsey = [[X90(q1), Id(q1, delay), X90(q1)] for delay in np.linspace(0.0, 1e-6, 11)]
         show(ramsey[2])
-        
+        return ramsey
+    
+    def test_compile(self):
+        seq = self.test_Ramsey()
+        LL, wfLib = Compiler.compile_sequences(seq)
+        assert(len(LL[self.q1]) == 11)
+        assert(len(LL[self.q1][0]) == 2)
+        print [len(miniLL) == 3 for miniLL in LL[self.q1][1:]]
+        assert( all([len(miniLL) == 3 for miniLL in LL[self.q1][1:]]) )
+        assert(len(wfLib[self.q1]) == 12) # 10 non-zero delays + X90 + TAZ
 
 class MultiQubit(unittest.TestCase):
 
@@ -25,7 +36,11 @@ class MultiQubit(unittest.TestCase):
         # seq = [X90(q1), X(q1)*Y(q2), CNOT(q1,q2), X(q2)+Xm(q2), Y(q1)*(X(q2)+Xm(q2)), MEAS(q1,q2)]
         seq = [X90(q1), X(q1)*Y(q2), CNOT(q1,q2), Xm(q2), Y(q1)*X(q2)]
         show(seq)
-        #compileSeq(seq)
+        return seq
+    
+    def test_compile(self):
+        seq = self.test_Operators()
+        LL, wfLib = Compiler.compile_sequence(seq)
     
     def test_align(self):
         q1 = Qubit('q1', piAmp=1.0, pi2Amp=0.5, pulseLength=30e-9)
