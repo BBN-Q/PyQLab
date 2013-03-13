07f579cce5962596aeb434844810410a23aba8b9
Colm Ryan <cryan@bbn.com>
2013-03-13 09:41:53 -0400

    Try to get a dictionary view manager going.
    Have heaps of trouble with the notifications from an Instance or WeakRef.

--- MeasFilters.py
+++ MeasFilters.py
@@ -2,11 +2,20 @@
 Measurement filters
 """
 
-from traits.api import HasTraits, Int, Float, List, Str
+from traits.api import HasTraits, Int, Float, List, Str, Dict, Bool, on_trait_change, Function
+import JSONHelpers
+import enaml
+
 
 class MeasFilter(HasTraits):
 	name = Str
-	channel = Int
+	channel = Int(1)
+	enabled = Bool(True)
+
+	def get_stack_view(self):
+		with enaml.imports():
+			from MeasFiltersViews import FilterStackView
+		return FilterStackView(myFilter=self)
 
 class DigitalHomodyne(MeasFilter):
 	boxCarStart = Int(0, desc='The start index of the integration window in pts.')
@@ -14,17 +23,37 @@ class DigitalHomodyne(MeasFilter):
 	IFfreq = Float(10e6, desc='The I.F. frequency for digital demodulation.')
 	samplingRate = Float(250e6, desc='The sampling rate of the digitizer.')
 
+
 class Correlator(MeasFilter):
 	filters = List(MeasFilter)
 
+class MeasFilterLibrary(HasTraits):
+	filterDict = Dict(Str, MeasFilter)
+	libFile = Str('MeasFilterLibrary.json', transient=True)
+	filterList = List([DigitalHomodyne, Correlator])
+
+	@on_trait_change('filterDict_items')
+	def trigger_callback(self, obj, name, new):
+		print('Decorator trait notifier triggered')
+
+	def write_to_file(self):
+		with open(self.libFile,'w') as FID:
+			json.dump(self, FID, cls=JSONHelpers.QLabEncoder, indent=2, sort_keys=True)
+
+	def load_from_file(self):
+		pass
+
+
+
 if __name__ == "__main__":
 
+	testFilter1 = DigitalHomodyne(name='M1', boxCarStart=100, boxCarStop=500, IFfreq=10e6, samplingRate=250e6, channel=1)
+	testFilter2 = DigitalHomodyne(name='M2', boxCarStart=150, boxCarStop=600, IFfreq=39.2e6, samplingRate=250e6, channel=2)
 
-	testFilter1 = DigitalHomodyne(name='M1', boxCarStart=100, boxCarStop=500, IFfreq=10e6, samplingRate=250e6)
-	testFilter2 = DigitalHomodyne(name='M2', boxCarStart=150, boxCarStop=600, IFfreq=39.2e6, samplingRate=250e6)
+	testLib = MeasFilterLibrary()
+	testLib.filterDict.update({'M1':testFilter1, 'M2':testFilter2})
 
-	import enaml
 	from enaml.stdlib.sessions import show_simple_view
 	with enaml.imports():
 		from MeasFiltersViews import MeasFilterManagerWindow
-	session = show_simple_view(MeasFilterManagerWindow(filters=[testFilter1, testFilter2]))
+	session = show_simple_view(MeasFilterManagerWindow(filterLib=testLib))
