9a6467688f567abc405a3fca6c4bfda7b6cd0351
Coherence Controller <qlab@bbn.com>
2013-04-24 10:14:18 -0400

    Handle filepaths in an OS independent manner.
    --CAR

--- FileWatcher.py
+++ FileWatcher.py
@@ -11,15 +11,15 @@ class MyEventHandler(FileSystemEventHandler):
         self.callback = callback
 
     def on_modified(self, event):
-        if event.src_path == self.filePath:
+        if os.path.normpath(event.src_path) == self.filePath:
             self.callback()
 
 class LibraryFileWatcher(object):
     def __init__(self, filePath, callback):
         super(LibraryFileWatcher, self).__init__()
-        self.filePath = filePath
+        self.filePath = os.path.normpath(filePath)
         self.callback = callback
-        self.eventHandler = MyEventHandler(filePath, callback)
+        self.eventHandler = MyEventHandler(self.filePath, callback)
         self.resume()
 
     def __del__(self):
