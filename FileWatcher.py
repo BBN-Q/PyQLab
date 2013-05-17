79e0f359640f4366505e34d13f77aa40af39b2a3
Colm Ryan <colm@colmryan.org>
2013-05-16 23:00:01 -0400

    Less conflicts in FileWatcher.
    Pause before trying to read fild. Don't start/stop observer and possible leak memory and threads.

--- FileWatcher.py
+++ FileWatcher.py
@@ -1,6 +1,7 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
+import time
 
 
 class MyEventHandler(FileSystemEventHandler):
@@ -9,10 +10,18 @@ class MyEventHandler(FileSystemEventHandler):
         super(MyEventHandler, self).__init__()
         self.filePath = filePath
         self.callback = callback
+        self.paused = True
 
     def on_modified(self, event):
         if os.path.normpath(event.src_path) == self.filePath:
-            self.callback()
+            if not self.paused:
+                """
+                Hold off for half a second
+                If the event is from the file being opened to be written this gives
+                time for it to be written.
+                """
+                time.sleep(0.5)
+                self.callback()
 
 class LibraryFileWatcher(object):
     def __init__(self, filePath, callback):
@@ -20,6 +29,9 @@ class LibraryFileWatcher(object):
         self.filePath = os.path.normpath(filePath)
         self.callback = callback
         self.eventHandler = MyEventHandler(self.filePath, callback)
+        self.observer = Observer()
+        self.watch = self.observer.schedule(self.eventHandler, path=os.path.dirname(self.filePath))
+        self.observer.start()
         self.resume()
 
     def __del__(self):
@@ -27,9 +39,7 @@ class LibraryFileWatcher(object):
         self.observer.join()
 
     def pause(self):
-        self.observer.stop()
+        self.eventHandler.paused = True
 
     def resume(self):
-        self.observer = Observer()
-        self.watch = self.observer.schedule(self.eventHandler, path=os.path.dirname(self.filePath))
-        self.observer.start()
+        self.eventHandler.paused = False
