62fc802918fd908a856b0289d8ccb584090bc61c
Colm Ryan <colm@colmryan.org>
2013-05-28 20:02:15 -0400

    Latest attempts at machine learning qubit measurement.
    PCA and a simple state vector machine with some nice plotting.

--- analysis/SSRO.py
+++ analysis/SSRO.py
@@ -1,16 +1,14 @@
 import numpy as np
-from scipy.io import loadmat
 from scipy.signal import butter, lfilter
 from scipy.io import loadmat
 
-from sklearn import decomposition, preprocessing
+from sklearn import decomposition, preprocessing, cross_validation
 from sklearn import svm, grid_search
 
 import pywt
+import h5py
 
-import glob
-
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def create_fake_data(SNR, dt, maxTime, numShots, T1=1.0):
 	"""
@@ -96,17 +94,15 @@ def test_nn(SNR):
 	pass
 
 
-def load_exp_data(path):
-	matFiles = glob.glob(path+'*.mat')
-	allRecords = None
-	for matFile in matFiles:
-		if allRecords is not None:
-			allRecords = np.dstack((allRecords, np.squeeze(loadmat(matFile)['demodSignal']) ))
-		else:
-			allRecords = np.squeeze(loadmat(matFile)['demodSignal'])
+def load_exp_data(fileName):
+	"""
+	Helper function to load data dumped from matlab
+	"""
+	f = h5py.File(fileName, 'r')
+	gData = np.fromstring(f['groundData'].value, dtype=np.complex).reshape(f['groundData'].shape)
+	eData = np.fromstring(f['excitedData'].value, dtype=np.complex).reshape(f['excitedData'].shape)
 
-	return allRecords[:,0,:], allRecords[:,1,:]
-
+	return gData, eData
 def wavelet_transform(measRecords, wavelet):
 	"""
 	Take and array of measurment records, wavelet transform and return the most significant components.
@@ -120,51 +116,65 @@ def wavelet_transform(measRecords, wavelet):
 if __name__ == '__main__':
 	pass
 
-	SNR = 1e4
-	fakeData = create_fake_data(SNR, 1e-3, 1, 4000)
-
-	#Use PCA to extract fewer useful features
-	pca = decomposition.PCA()
-	pca.n_components = 40
-	pca.fit(fakeData)
-
-	# trainData = pca.transform(fakeData)
-	# validateData =  pca.transform(create_fake_dpata(SNR, 1e-2, 1, 2000))
-
-	trueStates = np.tile([0,1], 2000).flatten()
-
-	# trueStates = np.hstack((np.zeros(1000), np.ones(1000)))
-	# testData = wavelet_transform(fakeData, 'db4')
-	testData = pca.transform(fakeData)
-	# scaler = preprocessing.Scaler().fit(testData)
-	# testData = scaler.transform(testData)
-
-	searchParams = {'gamma':(1.0/100)*np.logspace(-3, 0, 40), 'nu':np.arange(0.01, 0.2, 0.02)}
-	clf = grid_search.GridSearchCV(svm.NuSVC(cache_size=500), searchParams, n_jobs=1)
-	clf.fit(testData, trueStates)
+	# SNR = 1e4
+	# fakeData = create_fake_data(SNR, 1e-3, 1, 4000)
 
 
-	# fakeData = create_fake_data(SNR, 1e-2, 1, 5000)
+	# # trainData = pca.transform(fakeData)
+	# # validateData =  pca.transform(create_fake_dpata(SNR, 1e-2, 1, 2000))
+
+	# trueStates = np.tile([0,1], 2000).flatten()
+
+	# # trueStates = np.hstack((np.zeros(1000), np.ones(1000)))
+	# # testData = wavelet_transform(fakeData, 'db4')
+	# testData = pca.transform(fakeData)
+	# # scaler = preprocessing.Scaler().fit(testData)
+	# # testData = scaler.transform(testData)
+
+
+	# # fakeData = create_fake_data(SNR, 1e-2, 1, 5000)
 	# validateData = scaler.transform(validateData)
 
 
-	# numAvgs = 10
-	# fileName = '/home/cryan/Desktop/SSResults-2013-01-09-09-51-17.mat'
+	gData, eData = load_exp_data('/home/cryan/Desktop/SSData.mat')
 
-	# pulseData = extract_meas_data(fileName, numAvgs)
+	# #Use PCA to extract fewer, more useful features
+	allData = np.vstack((np.hstack((gData.real, gData.imag)), np.hstack((eData.real, eData.imag))))
+	pca = decomposition.PCA()
+	pca.n_components = 4
+	reducedData = pca.fit_transform(allData)
 
 
+	#Assing the assumed states
+	states = np.repeat([0,1], 10000)
 
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(reducedData, states, test_size=0.2, random_state=0)
 
-	# #Assumed states alternate between ground and excited.
-	# states = np.tile([0,1], (1, 400)).flatten()
+	# searchParams = {'gamma':(1.0/100)*np.logspace(-3, 0, 10), 'nu':np.arange(0.01, 0.2, 0.02)}
+	# clf = grid_search.GridSearchCV(svm.NuSVC(cache_size=2000), searchParams, n_jobs=2)
+	searchParams = {'C':np.linspace(0.1,4,20)}
+	clf = grid_search.GridSearchCV(svm.SVC(cache_size=2000), searchParams)
+	# clf = svm.SVC()
 
-	# plt.figure()
-	# plt.scatter(np.mean(pulseData[:,:30],1), np.mean(pulseData[:,31:],1), c=states, cmap=plt.cm.Paired)
+	clf.fit(X_train, y_train)
 
-	# plt.figure()
-	# plt.scatter(reducedData[:,0], reducedData[:,1], c=states, cmap=plt.cm.Paired)
+	print clf.score(X_test, y_test)
 
+	# gridScores =  np.reshape([x[1] for x in clf.grid_scores_], (clf.param_grid['nu'].size, clf.param_grid['gamma'].size))
+
+	# x_min, x_max = reducedData[:, 0].min() - 0.1, reducedData[:, 0].max() + 0.1
+	# y_min, y_max = reducedData[:, 1].min() - 0.1, reducedData[:, 1].max() + 0.1
+
+	# xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.005), np.arange(y_min, y_max, 0.005))
+	# Z = clf.predict(np.c_[xx.ravel(), yy.ravel(), np.zeros(xx.size), np.zeros(xx.size)])
+	# Z = Z.reshape(xx.shape)
+	# plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+	# plt.scatter(reducedData[:10000,0], reducedData[:10000,1], s=1, c='b', edgecolors='none')
+	# plt.scatter(reducedData[10000:,0], reducedData[10000:,1], s=1, c='r', edgecolors='none')
+	# plt.xlim((x_min, x_max))
+	# plt.ylim((y_min, y_max))
+	# plt.xlabel('Principal Component 1', fontsize=16)
+	# plt.ylabel('Principal Component 2', fontsize=16)
 	# plt.show()
 
 
