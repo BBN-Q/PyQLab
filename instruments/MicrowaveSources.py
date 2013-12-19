ce7bf38708b42d9ba44058ee57c6731119314b06
Colm Ryan <colm@colmryan.org>
2013-12-19 18:24:39 -0500

    Add Rhode and Schwarz SMIQ source class

--- instruments/MicrowaveSources.py
+++ instruments/MicrowaveSources.py
@@ -41,6 +41,12 @@ class Labbrick64(MicrowaveSource):
     gateMinWidth = Float(100e-9)
     gateDelay = Float(-60e-9)
 
+class RhodeSchwarzSMIQ03(MicrowaveSource):
+    refSource = Enum("Internal" , "External").tag(desc="Source of 10MHz ref.")
+    gateBuffer = Float(20e-9)
+    gateMinWidth = Float(100e-9)
+    gateDelay = Float(-60e-9)
+
 class HP8673B(MicrowaveSource):
     pass
 
@@ -48,7 +54,7 @@ class HP8340B(MicrowaveSource):
     pass
 
 #List of possible sources for other views
-MicrowaveSourceList = [AgilentN5183A, HolzworthHS9000, Labbrick, Labbrick64, HP8673B, HP8340B]
+MicrowaveSourceList = [AgilentN5183A, HolzworthHS9000, Labbrick, Labbrick64, RhodeSchwarzSMIQ03, HP8673B, HP8340B]
 
 if __name__ == "__main__":
     from MicrowaveSources import AgilentN5183A
