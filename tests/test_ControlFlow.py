42a6bb8fd09059b7b34a2c7502eeaded7dd52143
Blake R. Johnson <bjohnson@bbn.com>
2015-01-05 10:18:06 -0500

    Add WAITCMP instruction.
    Create it with qwait("CMP") to pause the sequencer until the comparison register
    has been updated.
    
    --BRJ and CAR

--- tests/test_ControlFlow.py
+++ tests/test_ControlFlow.py
@@ -20,6 +20,22 @@ class ControlFlow(unittest.TestCase):
         # print ([CmpEq(0), Goto(label(seq1))] + seq2 + [Goto(endlabel(seq1))] + seq1
         assert( qif(0, seq1, seq2) == [CmpEq(0), Goto(label(seq1))] + seq2 + [Goto(endlabel(seq1))] + seq1 )
 
+    def test_qif_single_element(self):
+        q1 = self.q1
+        # just if branch
+        seq = qif(0, X(q1))
+        # if and else branches
+        seq = qif(0, X(q1), Y(q1))
+
+    def test_inline_qif(self):
+        q1 = self.q1
+        seq = [X90(q1), Y(q1), qwait("CMP"), qif(0, [Id(q1)], [X(q1)]), Y(q1)]
+        Compiler.compile_sequence(seq)
+
+        seq = [X90(q1), Y(q1), qwait("CMP"), qif(0, [Id(q1)], [X(q1)]), Y(q1)]
+        seqs = [[Id(q1), Y(q1)], [X(q1), Y(q1)], seq]
+        Compiler.compile_sequences(seqs)
+
     def test_qwhile(self):
         q1 = self.q1
         seq1 = [X90(q1), Y90(q1)]
@@ -56,6 +72,12 @@ class ControlFlow(unittest.TestCase):
         seq1 = [X90(q1), Y90(q1)]
         label(seq1)
         assert( qrepeat(5, seq1) == [LoadRepeat(5)] + seq1 + [Repeat(label(seq1))] )
+
+    def test_qwait(self):
+        q1 = self.q1
+        seq1 = [qwait(), qwait("CMP")]
+        assert( seq1[0].instruction == "WAIT" )
+        assert( seq1[1].instruction == "WAITCMP" )
 
     def test_flatten_and_separate(self):
         seq = [1, ([2, ([3], [4, ([5, 6, 7], [8, 9])])], [10, 11])]
