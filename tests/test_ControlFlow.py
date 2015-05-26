5f0ef6e93a0bc671faec34efe630c44c2f2d3a87
Blake R. Johnson <bjohnson@bbn.com>
2015-05-26 14:00:28 -0400

    Refactor ControlFlow entries into individual types.
    Helps make dispatching on entry type less verbose.

--- tests/test_ControlFlow.py
+++ tests/test_ControlFlow.py
@@ -5,7 +5,7 @@ from QGL.ControlFlow import CmpEq, CmpNeq, Goto, Call, Return, LoadRepeat, Repea
 from QGL.BlockLabel import label, endlabel
 
 
-class ControlFlow(unittest.TestCase):
+class ControlFlowTest(unittest.TestCase):
     def setUp(self):
         self.q1 = Qubit(label='q1')
         self.q2 = Qubit(label='q2')
@@ -78,8 +78,8 @@ class ControlFlow(unittest.TestCase):
     def test_qwait(self):
         q1 = self.q1
         seq1 = [qwait(), qwait("CMP")]
-        assert( seq1[0].instruction == "WAIT" )
-        assert( seq1[1].instruction == "LOADCMP" )
+        assert( isinstance(seq1[0], ControlFlow.Wait) )
+        assert( isinstance(seq1[1], ControlFlow.LoadCmp) )
 
     def test_flatten_and_separate(self):
         seq = [1, ([2, ([3], [4, ([5, 6, 7], [8, 9])])], [10, 11])]
