b75acae0a55ad64ab900d50d2166ef450fa610d9
CoherenceController <qlab@bbn.com>
2015-12-22 13:42:06 -0500

    Fix attenuation sweep
    The DA channel was only updated in one direction (from json) and it was
    off by 1.
    --DR

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,4 +1,4 @@
 # from .dialogs import AddDialog
-from .enum_combos import EnumComboBox, EnumFloatComboBox
+from .enum_combos import EnumComboBox, EnumFloatComboBox, EnumIntComboBox
 from .qt_list_str_widget import QtListStrWidget
 # from .DictManager import DictManager, DictManagerView
