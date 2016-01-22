72121644df0cee42ff6c60d43057fa25543f8bdf
Blake R. Johnson <bjohnson@bbn.com>
2016-01-22 10:02:20 -0500

    Remove code that was moved to the translators.

--- instruments/drivers/APS2.py
+++ instruments/drivers/APS2.py
@@ -8,14 +8,7 @@ class APS2(AWG):
 	seqFileExt = Constant('.h5')
 	triggerSource = Enum('Internal', 'External', 'System').tag(desc='Source of trigger')
 
-	empty_channel_set = {'ch12':{}, 'ch12m1':{}, 'ch12m2':{}, 'ch12m3':{}, 'ch12m4':{}}
 	naming_convention = ['12', '12m1', '12m2', '12m3', '12m4']
-
-	def read_sequence_file(self, filename):
-		return read_APS2_file(filename)
-
-	def write_sequence_file(self, data, filename):
-		write_APS2_file(data, filename)
 
 class APS2TDM(AWG):
 	numChannels = Int(default=0)
