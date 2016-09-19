698353f9434b16e6e52d530f7bdd1df4d788a141
Graham Rowlands <grahamrow@gmail.com>
2016-09-19 17:51:17 -0400

    Snake case for digitizers

--- instruments/Digitizers.py
+++ instruments/Digitizers.py
@@ -12,61 +12,34 @@ from enaml.qt.qt_application import QtApplication
 class Digitizer(Instrument):
 	pass
 
-class AlazarATS9870(Digitizer):
-	address = Str('1').tag(desc='Location of the card') #For now we only have one
-	acquireMode = Enum('digitizer', 'averager').tag(desc='Whether the card averages on-board or returns single-shot data')
-	clockType = Enum('ref')
-	delay = Float(0.0).tag(desc='Delay from trigger')
-	samplingRate = Float(100000000).tag(desc='Sampling rate in Hz')
-	verticalScale = Float(1.0).tag(desc='Peak voltage (V)')
-	verticalOffset = Float(0.0).tag(desc='Vertical offset (V)')
-	verticalCoupling = Enum('AC','DC').tag(desc='AC/DC coupling')
-	bandwidth = Enum('20MHz', 'Full').tag(desc='Input bandwidth filter')
-	triggerLevel = Float(0.0).tag(desc='Trigger level (mV)')
-	triggerSource = Enum('A','B','Ext').tag(desc='Trigger source')
-	triggerCoupling = Enum('AC','DC').tag(desc='Trigger coupling')
-	triggerSlope = Enum('rising','falling').tag(desc='Trigger slope')
-	recordLength = Int(1024).tag(desc='Number of samples in each record')
-	nbrSegments = Int(1).tag(desc='Number of segments in memory')
-	nbrWaveforms = Int(1).tag(desc='Number of times each segment is repeated')
-	nbrRoundRobins = Int(1).tag(desc='Number of times entire memory is looped')
-
-	def json_encode(self, matlabCompatible=False):
-		if matlabCompatible:
-			"For the Matlab experiment manager we seperately nest averager, horizontal, vertical settings"
-			jsonDict = {}
-			jsonDict['address'] = self.address
-			jsonDict['deviceName'] = 'AlazarATS9870'
-			jsonDict['horizontal'] = {'delayTime':self.delay, 'samplingRate':self.samplingRate}
-			jsonDict['vertical'] = {k:getattr(self,k) for k in ['verticalScale', 'verticalOffset', 'verticalCoupling', 'bandwidth']}
-			jsonDict['trigger'] = {k:getattr(self,k) for k in ['triggerLevel', 'triggerSource', 'triggerCoupling', 'triggerSlope']}
-			jsonDict['averager'] = {k:getattr(self,k) for k in ['recordLength', 'nbrSegments', 'nbrWaveforms', 'nbrRoundRobins']}
-			#Add the other necessities
-			jsonDict['acquireMode'] = self.acquireMode
-			jsonDict['clockType'] = self.clockType
-		else:
-			jsonDict = super(AlazarATS9870, self).json_encode(matlabCompatible)
-
-		return jsonDict
+class ATS9870(Digitizer):
+	address           = Str('1').tag(desc='Location of the card') #For now we only have one
+	acquire_mode      = Enum('digitizer', 'averager').tag(desc='Whether the card averages on-board or returns single-shot data')
+	clock_type        = Enum('ref')
+	delay             = Float(0.0).tag(desc='Delay from trigger')
+	sampling_rate     = Float(100000000).tag(desc='Sampling rate in Hz')
+	vertical_scale    = Float(1.0).tag(desc='Peak voltage (V)')
+	vertical_offset   = Float(0.0).tag(desc='Vertical offset (V)')
+	vertical_coupling = Enum('AC','DC').tag(desc='AC/DC coupling')
+	bandwidth         = Enum('20MHz', 'Full').tag(desc='Input bandwidth filter')
+	trigger_level     = Float(0.0).tag(desc='Trigger level (mV)')
+	trigger_source    = Enum('A','B','Ext').tag(desc='Trigger source')
+	trigger_coupling  = Enum('AC','DC').tag(desc='Trigger coupling')
+	trigger_slope     = Enum('rising','falling').tag(desc='Trigger slope')
+	record_length     = Int(1024).tag(desc='Number of samples in each record')
+	nbr_segments      = Int(1).tag(desc='Number of segments in memory')
+	nbr_waveforms     = Int(1).tag(desc='Number of times each segment is repeated')
+	nbr_round_robins  = Int(1).tag(desc='Number of times entire memory is looped')
 
 class X6(Digitizer):
-	recordLength = Int(1024).tag(desc='Number of samples in each record')
-	nbrSegments = Int(1).tag(desc='Number of segments in memory')
-	nbrWaveforms = Int(1).tag(desc='Number of times each segment is repeated')
-	nbrRoundRobins = Int(1).tag(desc='Number of times entire memory is looped')
-	enableRawStreams = Bool(False).tag(desc='Enable capture of raw data from ADCs')
+	record_length      = Int(1024).tag(desc='Number of samples in each record')
+	nbr_segments       = Int(1).tag(desc='Number of segments in memory')
+	nbr_waveforms      = Int(1).tag(desc='Number of times each segment is repeated')
+	nbr_round_robins   = Int(1).tag(desc='Number of times entire memory is looped')
+	enable_raw_streams = Bool(False).tag(desc='Enable capture of raw data from ADCs')
 
-	digitizerMode = Enum('digitizer', 'averager').tag(desc='Whether the card averages on-board or returns single-shot data')
-	reference = Enum('external', 'internal').tag(desc='Clock source for 10MHz reference to clock generation tree')
-
-	def json_encode(self, matlabCompatible=False):
-		jsonDict = super(X6, self).json_encode(matlabCompatible)
-		if matlabCompatible:
-			# For the Matlab experiment manager we nest averager settings
-			map(lambda x: jsonDict.pop(x), ['recordLength', 'nbrSegments', 'nbrWaveforms', 'nbrRoundRobins'])
-			jsonDict['averager'] = {k:getattr(self,k) for k in ['recordLength', 'nbrSegments', 'nbrWaveforms', 'nbrRoundRobins']}
-
-		return jsonDict
+	digitizer_mode     = Enum('digitizer', 'averager').tag(desc='Whether the card averages on-board or returns single-shot data')
+	reference          = Enum('external', 'internal').tag(desc='Clock source for 10MHz reference to clock generation tree')
 
 if __name__ == "__main__":
 	from Digitizers import X6
