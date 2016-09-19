088e09580d18a806bc3d919f73492ac281602b72
Graham Rowlands <grahamrow@gmail.com>
2016-09-19 17:50:36 -0400

    Snake-case for measurement filters. Parity with pycontrol as much as possible.

--- MeasFilters.py
+++ MeasFilters.py
@@ -5,7 +5,7 @@ Measurement filters
 from atom.api import Atom, Int, Float, List, Str, Dict, Bool, Enum, Coerced, Typed, observe, Instance
 import enaml
 from enaml.qt.qt_application import QtApplication
-from instruments.Digitizers import AlazarATS9870, X6
+from instruments.Digitizers import ATS9870, X6
 from DictManager import DictManager
 import json
 import sys
@@ -14,29 +14,18 @@ from JSONLibraryUtils import FileWatcher, LibraryCoders
 class MeasFilter(Atom):
     label = Str()
     enabled = Bool(True)
-    dataSource = Str().tag(desc="Where the measurement data is pushed from.")
+    data_source = Str().tag(desc="Where the measurement data is pushed from.")
 
-    def json_encode(self, matlabCompatible=False):
+    def json_encode(self):
         jsonDict = self.__getstate__()
-        if matlabCompatible:
-            jsonDict['filterType'] = self.__class__.__name__
-            jsonDict.pop('enabled', None)
-            jsonDict.pop('label', None)
-            import numpy as np
-            import base64
-            try:
-                jsonDict['kernel'] = base64.b64encode(eval(self.kernel))
-            except:
-                jsonDict['kernel'] = []
-        else:
-            jsonDict['x__class__'] = self.__class__.__name__
-            jsonDict['x__module__'] = self.__class__.__module__
+        jsonDict['x__class__'] = self.__class__.__name__
+        jsonDict['x__module__'] = self.__class__.__module__
         return jsonDict
 
 class Plotter(MeasFilter):
     label    = Str()
     enabled  = Bool(True)
-    plotMode = Enum('amp/phase', 'real/imag', 'quad').tag(desc='Filtered data scope mode.')
+    plot_mode = Enum('amp/phase', 'real/imag', 'quad').tag(desc='Filtered data scope mode.')
 
 class WriteToHDF5(MeasFilter):
     label       = Str()
@@ -45,15 +34,15 @@ class WriteToHDF5(MeasFilter):
     compression = Bool(True)
 
 class AlazarStreamSelector(MeasFilter):
-    dataSource = Instance((str, AlazarATS9870))
+    data_source = Instance((str, ATS9870))
     channel    = Str().tag(desc="Which channel to select from the Alazar")
 
-    def json_encode(self, matlabCompatible=False):
+    def json_encode(self):
         jsonDict = super(AlazarStreamSelector, self).json_encode()
 
-        obj = jsonDict.pop('dataSource')
+        obj = jsonDict.pop('data_source')
         if obj:
-            jsonDict['dataSource'] = obj.label
+            jsonDict['data_source'] = obj.label
         return jsonDict
 
     def update_from_jsondict(self, jsonDict):
@@ -63,51 +52,30 @@ class AlazarStreamSelector(MeasFilter):
         #Convert the strings to ascii for Python 2
         if sys.version_info[0] < 3:
             for label,value in jsonDict.items():
-                if isinstance(value, unicode):
-                    setattr(self, label, value.encode('ascii'))
-                else:
-                    setattr(self, label, value)
+                if hasattr(self, label):
+                    if isinstance(value, unicode):
+                        setattr(self, label, value.encode('ascii'))
+                    else:
+                        setattr(self, label, value)
 
 class X6StreamSelector(MeasFilter):
-    dataSource              = Instance((str, X6))
-    label                   = Str()
-    enableDemodStream       = Bool(True).tag(desc='Enable demodulated data stream')
-    enableDemodResultStream = Bool(True).tag(desc='Enable demod result data stream')
-    enableRawResultStream   = Bool(True).tag(desc='Enable raw result data stream')
-    IFfreq                  = Float(10e6).tag(desc='IF Frequency')
-    demodKernel             = Str().tag(desc='Integration kernel vector for demod stream')
-    demodKernelBias         = Str("").tag(desc="Kernel bias for integrated demod stream")
-    rawKernel               = Str().tag(desc='Integration kernel vector for raw stream')
-    rawKernelBias           = Str("").tag(desc="Kernel bias for integrated raw stream")
-    threshold               = Float(0.0).tag(desc='Qubit state decision threshold')
-    thresholdInvert         = Bool(False).tag(desc="Invert thresholder output")
+    data_source                = Instance((str, X6))
+    label                      = Str()
+    stream_type                = Enum('Raw', 'Demodulated', 'Integrated').tag(desc='Which stream type to select.')
+    if_freq                    = Float(10e6).tag(desc='IF Frequency')
+    demod_kernel               = Str().tag(desc='Integration kernel vector for demod stream')
+    demod_kernel_bias          = Str("").tag(desc="Kernel bias for integrated demod stream")
+    raw_kernel                 = Str().tag(desc='Integration kernel vector for raw stream')
+    raw_kernel_bias            = Str("").tag(desc="Kernel bias for integrated raw stream")
+    threshold                  = Float(0.0).tag(desc='Qubit state decision threshold')
+    threshold_invert           = Bool(False).tag(desc="Invert thresholder output")
 
-    def json_encode(self, matlabCompatible=False):
+    def json_encode(self):
         jsonDict = super(X6StreamSelector, self).json_encode()
 
-        obj = jsonDict.pop('dataSource')
+        obj = jsonDict.pop('data_source')
         if obj:
-            jsonDict['dataSource'] = obj.label
-
-        if matlabCompatible:
-            import numpy as np
-            import base64
-            try:
-                jsonDict['demodKernel'] = base64.b64encode(eval(self.demodKernel))
-            except:
-                jsonDict['demodKernel'] = []
-            try:
-                jsonDict['demodKernelBias'] = base64.b64encode(np.array(eval(self.demodKernelBias), dtype=np.complex128))
-            except:
-                jsonDict['demodKernelBias'] = []
-            try:
-                jsonDict['rawKernel'] = base64.b64encode(eval(self.rawKernel))
-            except:
-                jsonDict['rawKernel'] = []
-            try:
-                jsonDict['rawKernelBias'] = base64.b64encode(np.array(eval(self.rawKernelBias), dtype=np.complex128))
-            except:
-                jsonDict['rawKernelBias'] = []
+            jsonDict['data_source'] = obj.label
         return jsonDict
 
     def update_from_jsondict(self, jsonDict):
@@ -117,48 +85,29 @@ class X6StreamSelector(MeasFilter):
         #Convert the strings to ascii for Python 2
         if sys.version_info[0] < 3:
             for label,value in jsonDict.items():
-                if isinstance(value, unicode):
-                    setattr(self, label, value.encode('ascii'))
-                else:
-                    setattr(self, label, value)
+                if hasattr(self, label):
+                    if isinstance(value, unicode):
+                        setattr(self, label, value.encode('ascii'))
+                    else:
+                        setattr(self, label, value)
 
 class DigitalDemod(MeasFilter):
-    IFfreq = Float(10e6).tag(desc='The I.F. frequency for digital demodulation.')
-    bandwidth = Float(5e6).tag(desc='Low-pass filter bandwidth')
-    samplingRate = Float(250e6).tag(desc='The sampling rate of the digitizer.')
-    phase = Float(0.0).tag(desc='Phase rotation to apply in rad.')
-    decimFactor1 = Int(1).tag(desc="First stage polyphase decimation (before multiplication with reference).")
-    decimFactor2 = Int(1).tag(desc="Second stage polyphase decimation (before IIR filter).")
-    decimFactor3 = Int(1).tag(desc="Third stage polyphase decimation (after IIR filter).")
+    if_freq         = Float(10e6).tag(desc='The I.F. frequency for digital demodulation.')
+    bandwidth      = Float(5e6).tag(desc='Low-pass filter bandwidth')
+    sampling_rate  = Float(250e6).tag(desc='The sampling rate of the digitizer.')
+    phase          = Float(0.0).tag(desc='Phase rotation to apply in rad.')
+    decim_factor_1 = Int(1).tag(desc="First stage polyphase decimation (before multiplication with reference).")
+    decim_factor_2 = Int(1).tag(desc="Second stage polyphase decimation (before IIR filter).")
+    decim_factor_3 = Int(1).tag(desc="Third stage polyphase decimation (after IIR filter).")
 
 class KernelIntegration(MeasFilter):
-    kernel = Str('').tag(desc="Integration kernel vector.")
-    bias = Float(0.0).tag(desc="Bias after integration.")
-    simpleKernel = Bool(True)
-    boxCarStart = Int(1)
-    boxCarStop = Int(1)
-    IFfreq = Float(10e6)
-    samplingRate = Float(250e6)
-
-    def json_encode(self, matlabCompatible=False):
-        jsonDict = super(KernelIntegration, self).json_encode(matlabCompatible)
-        if matlabCompatible:
-            # re-encode kernel in base64
-            jsonDict.pop('kernel')
-
-            import numpy as np
-            import base64
-            try:
-                if self.simpleKernel:
-                    kernel = np.hstack((np.zeros(self.boxCarStart, dtype=np.complex), np.ones(self.boxCarStop-self.boxCarStart)))
-                    kernel *= np.exp(1j*2*np.pi*self.IFfreq*np.arange(self.boxCarStop)/self.samplingRate)
-                    kernel = base64.b64encode(kernel)
-                else:
-                    kernel = base64.b64encode(eval(self.kernel))
-            except:
-                kernel = []
-            jsonDict['kernel'] = kernel
-        return jsonDict
+    kernel        = Str('').tag(desc="Integration kernel vector.")
+    bias          = Float(0.0).tag(desc="Bias after integration.")
+    simple_kernel = Bool(True)
+    box_car_start = Int(1)
+    box_car_stop  = Int(1)
+    if_freq       = Float(10e6)
+    sampling_rate = Float(250e6)
 
 class Correlator(MeasFilter):
     filters = List()
@@ -168,17 +117,17 @@ class Correlator(MeasFilter):
         if not self.filters:
             self.filters = [None, None]
 
-    def json_encode(self, matlabCompatible=False):
-        jsonDict = super(Correlator, self).json_encode(matlabCompatible)
+    def json_encode(self):
+        jsonDict = super(Correlator, self).json_encode()
         #For correlation filters return the filter list as a list of filter names
         filterList = jsonDict.pop('filters')
         jsonDict['filters'] = [item.label for item in filterList] if filterList else []
-        jsonDict['dataSource'] = ','.join(jsonDict['filters']) # correlator has mulitple data sources which are one and the same as the filter list
+        jsonDict['data_source'] = ','.join(jsonDict['filters']) # correlator has mulitple data sources which are one and the same as the filter list
         return jsonDict
 
 class StateComparator(MeasFilter):
-    threshold = Float(0.0)
-    integrationTime = Int(-1).tag(desc='Comparator integration time in decimated samples, use -1 for the entire record')
+    threshold        = Float(0.0)
+    integration_time = Int(-1).tag(desc='Comparator integration time in decimated samples, use -1 for the entire record')
 
 class MeasFilterLibrary(Atom):
     # filterDict = Dict(Str, MeasFilter)
@@ -234,14 +183,11 @@ class MeasFilterLibrary(Atom):
             except IOError:
                 print("No measurement library found.")
 
-    def json_encode(self, matlabCompatible=False):
-        if matlabCompatible:
-            return {label:filt for label,filt in self.filterDict.items() if filt.enabled}
-        else:
-            return {
-                "filterDict": {label:filt for label,filt in self.filterDict.items()},
-                "version": self.version
-            }
+    def json_encode(self):
+        return {
+            "filterDict": {label:filt for label,filt in self.filterDict.items()},
+            "version": self.version
+        }
 
 
 measFilterList = [DigitalDemod, KernelIntegration, Correlator, StateComparator,
